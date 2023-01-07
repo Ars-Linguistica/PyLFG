1b3049ba1fec6fce05da31bb4a7fa1f808b75985
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-07 19:58:52 +0100

    Update pos_tagger.py
    Added lexicon parser.

--- pos_tagger.py
+++ pos_tagger.py
@@ -13,6 +13,25 @@ def pos_tagger(sentence: str, lexicon: dict) -> str:
     result += ")"
     return result
 
+def parse_lexicon(filename):
+    lexicon = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            # Ignore empty lines and lines starting with '#'
+            if not line.strip() or line.startswith('#'):
+                continue
+
+            word, pos = line.split(':')
+            word = word.strip()
+            pos = pos.strip()
+
+            if word in lexicon:
+                lexicon[word].append(pos)
+            else:
+                lexicon[word] = [pos]
+    return lexicon
+
 # Example usage
+lexicon = parse_lexicon("lexicon.txt")
 sentence = "The cat sat on the mat"
 print(pos_tagger(sentence, lexicon))  # Output: "(S (NP (D the) (N cat)) (VP (V sat) (PP (P on) (NP (D the) (N mat)))))"
