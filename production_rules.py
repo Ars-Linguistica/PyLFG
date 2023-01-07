11b13e155bca943062e2c22cf601ad1e64efc2d7
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-07 17:34:50 +0100

    Create production_rules.py

---
+++ production_rules.py
@@ -0,0 +1,13 @@
+f_structure = {
+    "S": [("NP VP", "NP_VP"), ("S Conj S", "S_Conj_S"), ("S Sub S", "S_Sub_S"), ("S Wh S", "S_Wh_S")],
+    "NP": [("D N", "D_N"), ("PN", "PN"), ("NP PP", "NP_PP"), ("NP Poss", "NP_Poss")],
+    "VP": [("V", "V"), ("V NP", "V_NP"), ("VP PP", "VP_PP"), ("VP Adv", "VP_Adv"), ("VP Inf", "VP_Inf")],
+    "PP": [("P NP", "P_NP")],
+    "Conj": [("and", "and"), ("or", "or")],
+    "Sub": [("that", "that")],
+    "Wh": [("who", "who"), ("what", "what"), ("when", "when"), ("where", "where"), ("why", "why"), ("how", "how")],
+    "Adv": [("quickly", "quickly"), ("slowly", "slowly"), ("loudly", "loudly"), ("quietly", "quietly")],
+    "Inf": [("to V", "to_V")],
+    "Poss": [("'s NP", "'s_NP")]
+}
+
