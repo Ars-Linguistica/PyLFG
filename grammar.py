67fae25b584502a8f4d0a72205ef8c94865ee50b
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-07 17:56:45 +0100

    Update grammar.py
    Updated production rules.

--- grammar.py
+++ grammar.py
@@ -1,59 +1,13 @@
 # Types
 
 D = "Determiner"  # a, an, the
-N = "Noun"  # cat, mat, dog, book
-V = "Verb"  # sit, sleep, run, read
-P = "Preposition"  # on, in, under, with
-Adv = "Adverb"  # quickly, slowly, carefully
-Adj = "Adjective"  # big, small, red, blue
-Pro = "Pronoun"  # I, you, he, she, it, we, they
-Conj = "Conjunction"  # and, or, but
-
-# Production rules
-
-# Simple sentences
-S -> NP VP
-S -> NP VP Adv
-S -> NP VP PP
-S -> NP VP PP Adv
-
-# Noun phrases
-NP -> D N
-NP -> D N PP
-NP -> D N Adj
-NP -> D N Adj N
-NP -> D N PP Adj N
-NP -> D N Adj N PP
-NP -> Pro
-
-# Verb phrases
-VP -> V
-VP -> V NP
-VP -> V NP Adv
-VP -> V NP PP
-VP -> V NP PP Adv
-VP -> V NP NP
-VP -> V NP NP Adv
-VP -> V NP NP PP
-VP -> V NP NP PP Adv
-VP -> V PP
-
-# Prepositional phrases
-PP -> P NP
-
-# Compound sentences
-S -> S Conj S
-
-# Types
-
-D = "Determiner"  # a, an, the
-N = "Noun"  # cat, mat, dog, book, house
-V = "Verb"  # sit, sleep, run, read, live
-P = "Preposition"  # on, in, under, with, by
-Adv = "Adverb"  # quickly, slowly, carefully, happily
-Adj = "Adjective"  # big, small, red, blue, happy
-Pro = "Pronoun"  # I, you, he, she, it, we, they, me
-Conj = "Conjunction"  # and, or, but, because
+N = "Noun"  # cat, mat, dog, book, house, tree
+V = "Verb"  # sit, sleep, run, read, live, climb
+P = "Preposition"  # on, in, under, with, by, near
+Adv = "Adverb"  # quickly, slowly, carefully, happily, silently
+Adj = "Adjective"  # big, small, red, blue, happy, green
+Pro = "Pronoun"  # I, you, he, she, it, we, they, me, him, her
+Conj = "Conjunction"  # and, or, but, because, while
 
 # Production rules
 
@@ -97,3 +51,25 @@ S -> S Conj NP VP
 S -> S Conj NP VP Adv
 S -> S Conj NP VP PP
 S -> S Conj NP VP PP Adv
+
+# Modal auxiliaries
+VP -> Mod V
+VP -> Mod V NP
+VP -> Mod V NP Adv
+VP -> Mod V NP PP
+VP -> Mod V NP PP Adv
+VP -> Mod V NP NP
+VP -> Mod V NP NP Adv
+VP -> Mod V NP NP PP
+VP -> Mod V NP NP PP Adv
+VP -> Mod V PP
+
+Mod -> 'can'
+Mod -> 'could'
+Mod -> 'may'
+Mod -> 'might'
+Mod -> 'must'
+Mod -> 'shall'
+Mod -> 'should'
+Mod -> 'will'
+Mod -> 'would'
