58165625d0201d3666c380e8231b32893f63d54b
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-07 17:49:02 +0100

    Update grammar.py
    Updated rules

--- grammar.py
+++ grammar.py
@@ -1,26 +1,32 @@
 # Types
 
 D = "Determiner"  # a, an, the
-N = "Noun"  # cat, mat, dog
-V = "Verb"  # sit, sleep, run
-P = "Preposition"  # on, in, under
-Adv = "Adverb"  # quickly, slowly
-Adj = "Adjective"  # big, small
+N = "Noun"  # cat, mat, dog, book
+V = "Verb"  # sit, sleep, run, read
+P = "Preposition"  # on, in, under, with
+Adv = "Adverb"  # quickly, slowly, carefully
+Adj = "Adjective"  # big, small, red, blue
+Pro = "Pronoun"  # I, you, he, she, it, we, they
+Conj = "Conjunction"  # and, or, but
 
 # Production rules
 
+# Simple sentences
 S -> NP VP
 S -> NP VP Adv
 S -> NP VP PP
 S -> NP VP PP Adv
 
+# Noun phrases
 NP -> D N
 NP -> D N PP
 NP -> D N Adj
 NP -> D N Adj N
 NP -> D N PP Adj N
 NP -> D N Adj N PP
+NP -> Pro
 
+# Verb phrases
 VP -> V
 VP -> V NP
 VP -> V NP Adv
@@ -30,5 +36,10 @@ VP -> V NP NP
 VP -> V NP NP Adv
 VP -> V NP NP PP
 VP -> V NP NP PP Adv
+VP -> V PP
 
+# Prepositional phrases
 PP -> P NP
+
+# Compound sentences
+S -> S Conj S
