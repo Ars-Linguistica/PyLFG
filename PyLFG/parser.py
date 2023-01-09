8d3c02c6afc6a3e7ab3a5d23061b172db233cc90
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-09 13:40:47 +0100

    Update parser.py
    Added functionality

--- PyLFG/parser.py
+++ PyLFG/parser.py
@@ -2,48 +2,140 @@ import re
 
 from .parse_tree import LFGParseTree, LFGParseTreeNode
 
-def build_parse_tree(tokens, lexicon, production_rules):
+def build_parse_tree(tokens: List[str], grammar: Dict[str, List[str]]) -> LFGParseTree:
     """
-    Builds a parse tree for a given sequence of tokens using the given lexicon and production rules.
-
-    Parameters:
-    - tokens (List[str]): The sequence of tokens to build the parse tree for.
-    - lexicon (Dict[str, List[str]]): The lexicon to use for building the parse tree.
-    - production_rules (List[Tuple[str, List[str]]]): The production rules to use for building the parse tree.
-
+    Build a parse tree for a given list of tokens using the specified grammar rules.
+    
+    Args:
+    - tokens: list of strings, representing the words in the sentence to parse
+    - grammar: dictionary containing the grammar rules, where keys are non-terminal symbols and values
+    are lists of strings representing the productions for that non-terminal symbol
+    
     Returns:
-    - LFGParseTree: The built parse tree.
+    - LFGParseTree: object representing the parse tree for the input sentence
+    
     """
-    pass
+    # Initialize list of parse tree nodes, with a dummy node at the beginning
+    nodes = [LFGParseTreeNode(None, None)]
+    
+    # Iterate through the list of tokens
+    for token in tokens:
+        # Find all productions that can be created using the current token
+        next_productions = []
+        for non_terminal, productions in grammar.items():
+            if token in productions:
+                next_productions.append(non_terminal)
+        
+        # If no productions were found, this is a parse error
+        if not next_productions:
+            raise ValueError(f"No productions found for token: {token}")
+        
+        # Create new parse tree nodes for each possible production
+        new_nodes = []
+        for production in next_productions:
+            new_node = LFGParseTreeNode(production, token)
+            new_nodes.append(new_node)
+        
+        # Add the new nodes to the list of parse tree nodes
+        nodes.extend(new_nodes)
+    
+    # Return the parse tree rooted at the dummy node
+    return LFGParseTree(nodes[0])
 
-def parse_lfg(sentence, lexicon, production_rules):
+def validate_parse_tree(tree: LFGParseTree, grammar: dict) -> bool:
+"""Validate a parse tree according to the given grammar rules.
+Parameters:
+tree (LFGParseTree): The parse tree to validate.
+grammar (dict): A dictionary of the grammar rules. The keys are the left-hand sides of the rules and the values are the lists of right-hand sides.
+
+Returns:
+bool: True if the parse tree is valid according to the grammar, False otherwise.
+"""
+# Base case: If the tree is a leaf node, it must be a terminal symbol
+if tree.is_leaf():
+    return tree.label in grammar["terminals"]
+
+# Recursive case: Check that the node's label is a non-terminal and that its children's labels are in the list of valid rules
+if tree.label in grammar["nonterminals"]:
+    return all(validate_parse_tree(child, grammar) for child in tree.children)
+
+# If the tree is neither a leaf node nor a non-terminal, it is invalid
+return False
+
+
+def cyk_parse(sentence, grammar):
+"""
+Parse a sentence using the CYK algorithm.
+Parameters:
+sentence (str): The sentence to parse.
+grammar (dict): A dictionary of production rules, where the keys are the left-hand sides of the rules and the values are lists of right-hand sides.
+
+Returns:
+list[LFGParseTree]: A list of parse trees, one for each valid parse of the sentence. If the sentence is not in the language defined by the grammar, an empty list is returned.
+"""
+
+# Tokenize the input
+words = sentence.split()
+n = len(words)
+
+# Initialize the parse table
+table = [[set() for i in range(n - j)] for j in range(n)]
+
+# Fill in the base cases
+for i in range(n):
+    for production in grammar.values():
+        for rhs in production:
+            if rhs == words[i]:
+                table[0][i].add(rhs)
+
+# Fill in the rest of the table
+for j in range(1, n):
+    for i in range(n - j):
+        for k in range(j):
+            for symbol1 in table[k][i]:
+                for symbol2 in table[j - k - 1][i + k + 1]:
+                    for lhs, rhs in grammar.items():
+                        if (symbol1, symbol2) in rhs:
+                            table[j][i].add(lhs)
+
+# Generate the parse trees
+parse_trees = []
+for lhs in table[-1][0]:
+    parse_tree = build_parse_tree(words, table, lhs, 0, n - 1, grammar)
+    if parse_tree is not None:
+        parse_trees.append(parse_tree)
+
+return parse_trees
+
+
+
+def parse_lfg(sentence: str, grammar: dict, memo: dict = {}) -> Iterator[LFGParseTree]:
     """
-    Parses a given sentence using the given lexicon and production rules.
-
-    Parameters:
-    - sentence (str): The sentence to parse.
-    - lexicon (Dict[str, List[str]]): The lexicon to use for parsing the sentence.
-    - production_rules (List[Tuple[str, List[str]]]): The production rules to use for parsing the sentence.
-
-    Returns:
-    - LFGParseTree: The parse tree for the sentence.
+    Parse the given sentence using the given context-free grammar.
+    Yields all valid parse trees as LFGParseTree objects.
+    Uses memoization to avoid recomputing parse trees for the same input.
     """
-    tokens = re.findall(r'\b\w+\b', sentence)
-    return build_parse_tree(tokens, lexicon, production_rules)
+    if sentence in memo:
+        yield from memo[sentence]
+    elif not sentence:
+        yield LFGParseTree(symbol="ROOT")
+    else:
+        for i, word in enumerate(sentence.split()):
+            if word in grammar:
+                for production in grammar[word]:
+                    for parse in parse_lfg(sentence[i + 1:], grammar, memo):
+                        yield LFGParseTree(symbol=production, children=(LFGParseTree(symbol=word), parse))
+        memo[sentence] = list(parse_lfg(sentence, grammar, memo))
 
-def parse_sentence(sentence, lexicon, production_rules):
+
+def parse_sentence(sentence: str, grammar: Grammar, max_parses: int = 1) -> Union[LFGParseTree, List[LFGParseTree]]:
     """
-    Parses a given sentence and returns the list of all possible parse trees.
-
-    Parameters:
-    - sentence (str): The sentence to parse.
-    - lexicon (Dict[str, List[str]]): The lexicon to use for parsing the sentence.
-    - production_rules (List[Tuple[str, List[str]]]): The production rules to use for parsing the sentence.
-
-    Returns:
-    - List[LFGParseTree]: The list of all possible parse trees for the sentence.
+    Parse the given sentence using the given grammar. If `max_parses` is 1, return a single parse tree. If `max_parses`
+    is greater than 1, return a list of up to `max_parses` parse trees.
     """
-    pass
+    words = sentence.split()
+    return parse_lfg(words, grammar, max_parses)
+
 
 def pos_tagger(sentence: str, lexicon: dict) -> str:
     """
@@ -76,17 +168,38 @@ def pos_tagger(sentence: str, lexicon: dict) -> str:
 
     return generate_parses(tags)
 
-def parse_lexicon(filename):
+def parse_lexicon(filename: str) -> Dict[str, List[str]]:
     """
-    Parses a lexicon file and returns the resulting lexicon as a dictionary.
-
-    Parameters:
-    - filename (str): The name of the file to parse.
-
+    Load the lexicon from the given file and return it as a dictionary.
+    
+    The file is expected to contain one word and its part of speech tag per line, separated by a colon.
+    Lines starting with '#' and empty lines are ignored.
+    
+    If a word appears multiple times in the file, all its tags are stored in a list in the dictionary.
+    
+    Args:
+        filename (str): The name of the file to load the lexicon from.
+    
     Returns:
-    - Dict[str, List[str]]: The resulting lexicon.
+        Dict[str, List[str]]: The lexicon as a dictionary.
+    
+    Examples:
+        >>> parse_lexicon('lexicon.txt')
+        {'the': ['D'], 'cat': ['N'], 'sat': ['V'], 'on': ['P'], 'mat': ['N'], 'fish': ['V', 'N']}
     """
-    pass
+    lexicon = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            # Ignore empty lines and lines starting with '#'
+            if not line.strip() or line.startswith('#'):
+                continue
 
+            word, pos = line.split(':')
+            word = word.strip()
+            pos = pos.strip()
 
-
+            if word in lexicon:
+                lexicon[word].append(pos)
+            else:
+                lexicon[word] = [pos]
+    return lexicon
