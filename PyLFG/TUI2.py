5f1c2fc59bcf1e017a4f44b9876d143dbdfb0e1b
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-13 03:56:41 +0100

    Update TUI2.py

--- PyLFG/TUI2.py
+++ PyLFG/TUI2.py
@@ -2,7 +2,7 @@ import textual
 import json
 from xlfg import parse_rule, parse_lexicon_entry, match_constraints, impose_constraints_in_tree, remove_unused_constraints
 import d3
-
+from command import process
 # Create a Textual App and a MainView
 app = textual.Application("PyLFG")
 main_view = textual.MainView()
@@ -17,63 +17,7 @@ def process_input():
     if input_string[0] == "$":
         # Process command
         command = input_string[1:]
-        if command == "display_rules":
-            print("Current production rules:")
-            for rule in rule_list.items:
-                print(rule.text)
-        elif command == "display_lexicon":
-            print("Current lexicon entries:")
-            for entry in lexicon_list.items:
-                print(entry.text)
-        elif command.startswith("load_grammar"):
-            filepath = command.split(" ")[1]
-            try:
-                with open(filepath, "r") as f:
-                    grammar = json.load(f)
-                for rule in grammar:
-                    lhs, rhs, c_structure_constraints = parse_rule(rule)
-                    rule_list.append(f"{lhs} → {' '.join(rhs)} {c_structure_constraints}")
-                print(f"Grammar loaded from {filepath}")
-            except Exception as e:
-                print(f"Error loading grammar from {filepath}: {e}")
-        elif command.startswith("load_lexicon"):
-            filepath = command.split(" ")[1]
-            try:
-                with open(filepath, "r") as f:
-                    lexicon = json.load(f)
-                for entry in lexicon:
-                    functional_labels = parse_lexicon_entry(entry)
-                    lexicon_list.append(f"{functional_labels}")
-                print(f"Lexicon loaded from {filepath}")
-            except Exception as e:
-                print(f"Error loading lexicon from {filepath}: {e}")
-        elif command.startswith("save_grammar"):
-            filepath = command.split(" ")[1]
-            try:
-                grammar = [rule.text for rule in rule_list.items]
-                with open(filepath, "w") as f:
-                    json.dump(grammar, f)
-                print(f"Grammar saved to {filepath}")
-            except Exception as e:
-                print(f"Error saving grammar to {filepath}: {e}")
-        elif command.startswith("save_lexicon"):
-            filepath = command.split(" ")[1]
-            try:
-                lexicon = [entry.text for entry in lexicon_list.items]
-                with open(filepath, "w") as f:
-                    json.dump(lexicon, f)
-                print(f"Lexicon saved to {filepath}")
-            except Exception as e:
-                print(f"Error saving lexicon to {filepath}: {e}")
-        elif command == "f-structure":
-                        if parse_tree is not None:
-                f_structure = impose_constraints_in_tree(remove_unused_constraints(parse_tree, rule_list.items, lexicon_list.items), rule_list.items, lexicon_list.items)
-                print("F-structure:")
-                print(f_structure)
-            else:
-                print("No sentence has been analyzed yet.")
-        else:
-            print("Invalid command")
+        process(command)        
     else:
         # Analyze sentence
         parse_tree = parse_sentence(input_string, rule_list.items, lexicon_list.items)
