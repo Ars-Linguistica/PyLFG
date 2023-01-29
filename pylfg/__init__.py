8fa3ffb0b69f7fc617b50caf2f2a6aee9ea59bea
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-29 02:47:56 +0100

    Update __init__.py

--- pylfg/__init__.py
+++ pylfg/__init__.py
@@ -1,3 +1,12 @@
 
 from .parser import *
 from .parse_tree import *
+
+__author__ = """SekouDiaoNlp"""
+__email__ = 'diao.sekou.nlp@gmail.com'
+__version__ = '0.1.1a0'
+__copyright__ = "Copyright (c) 2023, SekouDiaoNlp"
+__credits__ = ("Sekou Diao",)
+__license__ = "MIT"
+__maintainer__ = "SekouDiaoNlp"
+__status__ = "Alpha"
