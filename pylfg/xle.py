623dad0f582138b92a097b98f62bb24cae8a4228
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-29 02:34:20 +0100

    Update xle.py

--- pylfg/xle.py
+++ pylfg/xle.py
@@ -120,11 +120,12 @@ def load_fst(fst_path: str) -> fst.Fst:
     return fst
     
 def integrate_lfg(lexicon, grammar):
+    """
     Integrates a lexicon and grammar into a LFG (Lexical Functional Grammar) format.
     It loads templates, feature declaration, morphconfig and rules from specific files.
     Replaces placeholders in the templates with the parsed lexicon and grammar.
-Returns the LFG in string format.
-"""
+    Returns the LFG in string format.
+    """
     # Load the templates from common.templates.lfg
     with open('common.templates.lfg', 'r') as f:
         templates = f.read()
